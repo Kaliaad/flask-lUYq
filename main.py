99b21517edc479d2fa667fa041762606e0ea058f
Kaliaad <99507224+Kaliaad@users.noreply.github.com>
2023-12-09 11:25:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ def home():
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Discord OAuth Login</title>
+        <link rel="stylesheet" type="text/css" href="templates\style.css">
     </head>
     <body>
         <h1>Discord OAuth Login</h1>
